ce69b16e389f1478f5209da4e9620ccf057d2e1b
Tyler D <130164977+tdaniel12299@users.noreply.github.com>
2024-01-10 21:02:28 -0500

    ACTING WEIRD

--- main.js
+++ main.js
@@ -1,51 +1,17 @@
-//get player choice
+//get plchoice
 //get comp choice
 //determine winner 
 
 const cOptions = ["rock", "paper", "scissors"];
-let playerScore = 0;
-let compScore = 0;
-// let playerChoice = prompt("Please enter rock, paper, or scissors: ");
-let getComputerChoice = cOptions[Math.floor(Math.random() * cOptions.length)];
+const humanScore = document.getElementById("humanScore");
+const aiScore = document.getElementById("aiScore")
 
+let playGame = (playerChoice) => {
 
-// if (playerChoice != 'rock' || 'paper' || 'scissors') {
-//     console.log()
-// } else {
-//     console.log('Invalid option, please choose again')
-// };
+    const computerChoice = cOptions[Math.floor(Math.random() * 3)];
+    let result = "";
 
-let singleRound = (playerChoice, getComputerChoice) => {
-    switch (true) {
-        case playerChoice === getComputerChoice:
-            return 'Its a Draw!'
-        case playerChoice === 'rock' && getComputerChoice === 'paper':
-            return compScore++  
-        case playerChoice === 'rock' && getComputerChoice === 'scissors':
-            return  playerScore++
-        case playerChoice === 'scissors' && getComputerChoice === 'rock':
-            return compScore++ 
-        case playerChoice === 'scissors' && getComputerChoice === 'paper':
-            return  playerScore++  
-        case playerChoice === 'paper' && getComputerChoice === 'rock':
-            return  playerScore++ 
-        case playerChoice === 'paper' && getComputerChoice === 'scissors':
-            return compScore++ 
-    }
-}
-
-// const getScore = () => {
-//     
-// }
-
-const game = () => {
-    console.log(playerChoice);
-    console.log(getComputerChoice);
-    console.log(singleRound(playerChoice, getComputerChoice));
-    // console.log(playerScore)
-    
-}
-
-
-
-game();
+    if(playerChoice === computerChoice){
+        result = "Stalemale Tie!"
+    } else {
+        switch(playerChoice){}
